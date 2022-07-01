fb5c173c712b4d996b1f910df1ab46700ad141a7
Tunegee23 <ogunsanya.opeyemi23@gmail.com>
2022-06-30 19:32:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
 * main -Entry pont
-* Return: Always(0)
+* Return:Always(0)
  */
-int main(void)
+./betty-style.p1 int main(void)
 {
 puts ("\"Programming is like building a multilingual puzzle");
 return(0);
