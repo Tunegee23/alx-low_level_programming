df5c9fc866495dee0f1df3f4787bf0f540c090bb
Tunegee23 <ogunsanya.opeyemi23@gmail.com>
2022-07-01 11:58:46 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (success)
+ * main - Entry pont
+ * Return:Always(0)
  */
 int main(void)
 {
-Printf("with proper grammer, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
